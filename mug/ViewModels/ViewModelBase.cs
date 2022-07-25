9439a1a7f37251c67fbd52e521485b7922979cdb
RicardoSung <ricardo1966346317@gmail.com>
2022-07-25 14:54:13 +0800

    create mug-dotnet rider project

---
+++ mug/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using ReactiveUI;
+
+namespace mug.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
---
+++ mug/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace mug.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
